6f67494f48bf299684c7733f5b7e35131a19c70c
Laurence <lgwillmore@gmail.com>
2019-10-13 21:49:26 +0200

    header styling

--- layouts/partials/PageHeader.vue
+++ layouts/partials/PageHeader.vue
@@ -11,13 +11,13 @@
       <b-row class="border-bottom navigation">
         <b-col v-if="!isMobile()" class="p-l-auto"/>
         <b-col class="nav-link border-left" cols="4" md="1">
-          <nuxt-link to="/">home</nuxt-link>
+          <nuxt-link to="/" tag="button">home</nuxt-link>
         </b-col>
         <b-col class="nav-link border-left" cols="4" md="1">
-          <nuxt-link to="/blog">blog</nuxt-link>
+          <nuxt-link to="/blog" tag="button">blog</nuxt-link>
         </b-col>
         <b-col class="nav-link border-left border-right" cols="4" md="1">
-          <nuxt-link to="/oss">oss</nuxt-link>
+          <nuxt-link to="/oss" tag="button">oss</nuxt-link>
         </b-col>
         <b-col v-if="!isMobile()" class="p-r-auto"/>
 
@@ -42,7 +42,7 @@
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
     font-weight: 300;
     font-size: 40px;
-    color: #35495e;
+    color: #47494e;
     background: lightgray;
     letter-spacing: 1px;
     text-align: center;
@@ -50,9 +50,43 @@
 
   .nav-link {
     text-align: center;
+    color: #47494e;
+    padding: 0;
+  }
+
+  .nav-link:hover {
+    background: darkgray;
+    text-align: center;
+    color: #47494e;
+  }
+
+  nuxt-link {
+    color: #47494e;
   }
 
   .navigation {
     background: white;
   }
+
+  .nav-link button {
+    background: white;
+    border: 0;
+    padding: 0;
+    margin: 0;
+    outline: none;
+    width: 100%;
+  }
+
+  .nuxt-link-exact-active {
+    background: lightgray !important;
+    text-decoration: underline;
+  }
+
+
+  .nav-link button:hover {
+  @extend nav-link button;
+    background: darkgray;
+  }
+
+
 </style>
